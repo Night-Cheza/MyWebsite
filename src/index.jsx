9ac421761cb2bb1bf0b99c8d639bbddf91313f4d
Leila <leila.kassi@outlook.com>
2023-12-28 18:02:03 -0700

    fixed and removed unnecessary import

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,9 @@ import ReactDOM from 'react-dom/client';
 import React from 'react';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 // @ts-ignore
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Router>
         <App />        
-    </Router>
 );
